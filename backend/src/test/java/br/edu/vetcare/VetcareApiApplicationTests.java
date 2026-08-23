3ca157fa8f1a0d8110502253581b228c97a97846
w-torresdev <williantorreskk@gmail.com>
2026-08-22 22:02:30 -0300

    feat: implementa VetcareApiApplicationTests.java

---
+++ backend/src/test/java/br/edu/vetcare/VetcareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.vetcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetcareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
